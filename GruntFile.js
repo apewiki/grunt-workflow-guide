6893cffa4b225b8ddcdf7fa7879acd7241c7137e
Qin Zhu <zhu1996@yahoo.com>
2015-06-22 20:58:54 -0400

    Set up config directory, load all modules

--- GruntFile.js
+++ GruntFile.js
@@ -1,19 +1,24 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-contrib-concat');
+	//grunt.loadNpmTasks('grunt-sass');
+	//grunt.loadNpmTasks('grunt-contrib-concat');
+
+	//All installed grunt modules  are loaded here
+	require('load-grunt-tasks')(grunt);
+
+	var config = grunt.file.readYAML('GruntFile.yml');
 
 	grunt.initConfig({
 		sass: {
 			dist: {
-				src: 'src/sass/style.scss',
-				dest: 'dist/css/style.css'
+				src: config.scssDir + 'style.scss',
+				dest: config.cssDir + 'style.css'
 			}
 		},
 
 		concat: {
 			dist: {
-				src: 'src/js/*.js',
-				dest: 'dist/js/app.js'
+				src: config.jsSrcDir + '*.js',
+				dest: config.jsConcatDir + 'app.js'
 			}
 		}
 	});
